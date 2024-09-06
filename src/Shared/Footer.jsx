a27eedde60a65f9b8b2ba845503280fe3e7a38ad
Tufazzul1 <tufajjul2017@gmail.com>
2024-09-06 18:25:14 +0600

    created basic setup

---
+++ src/Shared/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">Hello From Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            Hello From Navbar
+            
+        </div>
+    );
+};
+
+export default Navbar;
+
+
